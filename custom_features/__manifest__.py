da766a1a0274ae2a4842b1b933ff66c7edc81422
mahmoodhoroun <horounmahmood@gmail.com>
2024-10-18 10:33:17 +0300

    Update

--- custom_features/__manifest__.py
+++ custom_features/__manifest__.py
@@ -4,6 +4,9 @@
     'depends': ['base', 'stock'],  # Include other dependencies as needed
     'data': [
         # Add your XML files here if any
+        'views/report_sale_order.xml',
+        'views/report_templates.xml',
+        'views/report_invoice.xml',
     ],
     'installable': True,
     'application': True,
